926786f8edf5822559a0efb7e1c2efa1121340ee
maaaason <ck10000682@gmail.com>
2017-04-15 09:45:52 +0800

    Add files via upload

--- multi HW2.R
+++ multi HW2.R
@@ -1,10 +1,10 @@
 #1-a
-data <- read.csv("D:\\a.csv", header = F)
+data1 <- read.csv("D:\\a.csv", header = F)
 S <- matrix(c(7476.45, 303.62, 303.62, 26.19),2,2)
 S_eiva <- eigen(S)$values
 S_eive <- eigen(S)$vectors
-y1_S <- S_eive[1,1] * data[,1] + S_eive[2,1] * data[,2]
-y2_S <- S_eive[1,2] * data[,1] + S_eive[2,2] * data[,2]
+y1_S <- S_eive[1,1] * data1[,1] + S_eive[2,1] * data1[,2]
+y2_S <- S_eive[1,2] * data1[,1] + S_eive[2,2] * data1[,2]
 y1_S_var <- var(y1_S)
 y2_S_var <- var(y2_S)
 
@@ -18,13 +18,12 @@ r_y1_x2 <- S_eive[2,1] * sqrt(S_eiva[1])/sqrt(S[2,2])
 #2-a
 corr <- 303.62/(sqrt(7476.45)*sqrt(26.19))
 R <- matrix(c(1,corr,corr,1),2,2)
-Z1 <- (data[,1]-mean(data[,1]))/sd(data[,1])
-Z2 <- (data[,2]-mean(data[,2]))/sd(data[,2])
-data_std <- matrix(c(Z1,Z2),10,2)
+Z1 <- (data1[,1]-mean(data1[,1]))/sd(data1[,1])
+Z2 <- (data1[,2]-mean(data1[,2]))/sd(data1[,2])
 R_eiva <- eigen(R)$values
 R_eive <- eigen(R)$vectors
-y1_R <- R_eive[1,1] * Z_1+R_eive[2,1] * Z_2
-y2_R <- R_eive[1,2] * Z_1+R_eive[2,2] * Z_2
+y1_R <- R_eive[1,1] * Z1+R_eive[2,1] * Z2
+y2_R <- R_eive[1,2] * Z1+R_eive[2,2] * Z2
 y1_R_var <- var(y1_R)
 y2_R_var <- var(y2_R)
 
@@ -35,6 +34,19 @@ Q2_b <- y1_R_var/(y1_R_var + y2_R_var)
 r_y1_z1 <- R_eive[1,1] * sqrt(R_eiva[1])
 r_y1_z2 <- R_eive[2,1] * sqrt(R_eiva[1])
 
-
-
+#4-a
+data4 <- read.csv("D:\\c.csv", header = F)
+data4_std <- matrix(0,19,12)
+for(i in 1:12){
+  data4_std[,i]=(data4[,i]-mean(data4[,i]))/sd(data4[,i])
+}
+R4 <- matrix(0,12,12)
+for(j in 1:12){
+  for(k in 1:12){
+    R4[j,k] <- cor(data4_std[,j],data4_std[,k])
+  }
+}
+R4_eiva <- eigen(R4)$values
+plot(x=1:12,y=R4_eiva)
+## 3 components are enough to effectively summarize the sample variability.
             
