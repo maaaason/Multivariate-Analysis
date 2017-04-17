71d37cff8626b848f0258466abed8084737b5bda
maaaason <ck10000682@gmail.com>
2017-04-17 18:16:20 +0800

    Add files via upload

--- multi HW2.R
+++ multi HW2.R
@@ -34,6 +34,67 @@ Q2_b <- y1_R_var/(y1_R_var + y2_R_var)
 r_y1_z1 <- R_eive[1,1] * sqrt(R_eiva[1])
 r_y1_z2 <- R_eive[2,1] * sqrt(R_eiva[1])
 
+#3-a(S)
+data3 <- read.csv("D:\\b.csv", header = T)
+S3 <- matrix(0,7,7)
+for(i in 1:7){
+  for(j in 1:7){
+    S3[i,j] <- cov(data3[,i],data3[,j])
+  }
+}
+S3_eiva <- eigen(S3)$values
+S3_eive <- eigen(S3)$vectors
+plot(x=1:7,y=S3_eiva)
+## 2 components are enough
+
+#3-b(S)
+## The first principal component appears to put much more emphasis on YrHgt and the second principal component appears to contrast BkFat with SaleHt.
+
+#3-d(S)
+Y3_S <- matrix(0,76,2)
+for(i in 1:7){
+  Y3_S[,1] <- S3_eive[1,i] * data3[,1]
+  Y3_S[,2] <- S3_eive[2,i] * data3[,2]
+}
+plot(y=Y3_S[,1],x=Y3_S[,2],xlab=expression(hat(y2)),ylab=expression(hat(y1)))
+## There are 3 outliers lie in lower right of the plot.
+
+#3-e(S)
+qqnorm(Y3_S[,1])
+## The samples of the first principal component are almost following normal distribution.
+
+#3-a(R)
+data3_std <- matrix(0,76,7)
+for(i in 1:7){
+  data3_std[,i]=(data3[,i]-mean(data3[,i]))/sd(data3[,i])
+}
+R3 <- matrix(0,7,7)
+for(j in 1:7){
+  for(k in 1:7){
+    R3[j,k] <- cor(data3_std[,j],data3_std[,k])
+  }
+}
+R3_eiva <- eigen(R3)$values
+R3_eive <- eigen(R3)$vectors
+plot(x=1:7,y=R3_eiva)
+## 3 components are enough
+
+#3-b(R)
+## The first principal component appears to contrast FtFrBody and PrctFFB with BkFat and the second principal component appears to be essentially a weighted average of YrHgt, PrctFFB, BkFat and SaleWt.
+
+#3-d(R)
+Y3_R <- matrix(0,76,2)
+for(i in 1:7){
+  Y3_R[,1] <- R3_eive[1,i] * data3[,1]
+  Y3_R[,2] <- R3_eive[2,i] * data3[,2]
+}
+plot(y=Y3_R[,1],x=Y3_R[,2],xlab=expression(hat(y2)),ylab=expression(hat(y1)))
+## There are 3 outliers lie in upper left of the plot.
+
+#3-e(R)
+qqnorm(Y3_R[,1])
+## The samples of the first principal component are not following normal distribution.
+
 #4-a
 data4 <- read.csv("D:\\c.csv", header = F)
 data4_std <- matrix(0,19,12)
@@ -49,4 +110,10 @@ for(j in 1:12){
 R4_eiva <- eigen(R4)$values
 plot(x=1:12,y=R4_eiva)
 ## 3 components are enough to effectively summarize the sample variability.
+
+#4-b
+R4_eive <- eigen(R4)$vectors
+## The first principal component put very much emphasis on the X3 statement.
+## The second principal component appears to reveal the negative effect of the X1, X6, X8, X11 and X12 statements.
+## The third principal component appears to contrast the X2 and X12 with the X4 statements.
             
